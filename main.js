c78430c2f3ed1a27023b31dd5e3a26191e59e723
Carlos Soltero <carlosh95@gmail.com>
2015-08-14 15:40:21 -0400

    cleaning ipage issues

--- main.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){
-	$(".social img").on("mouseover", function(){
-		$(this).css("width", "70%");
-	}).mouseout(function() {
-	    $(this).css("width", "80%");
-	});
-})
