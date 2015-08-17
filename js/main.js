b454a804c786c8aeb74d650db0d9a989e973c566
Carlos Soltero <carlosh95@gmail.com>
2015-08-16 22:56:31 -0400

    as of now the page works, i have to erase index1 and also go trhough the code in the css to make sure there is nothing repeating. I also have to add the functionality for the drop down meny for responsive web

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 
 $(document).ready(function(){
-	$(".social img").on("mouseover", function(){
-		$(this).css("width", "70%");
-	}).mouseout(function() {
-	    $(this).css("width", "80%");
-	});
+	// hover effect for social icons
+		$(".social img").on("mouseover", function(){
+			$(this).css("width", "70%");
+		}).mouseout(function() {
+		    $(this).css("width", "80%");
+		});
 
+
+	
 	$('#nav-toggle').click(function(){
 		$(this).toggleClass("active");
 	});
-	
+
 })
